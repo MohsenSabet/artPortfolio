b746af1235e0d607e136c1b324a866c2ebd789c7
Mohsen Sabet <msabetkoohenjani@myseneca.ca>
2025-06-13 12:58:05 -0400

    feat: Add full-page loading overlay and loader animations to improve user experience during route transitions

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,42 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '@/styles/globals.css';
 import Layout from '@/components/layout';
 import { ThemeProvider } from '@/context/ThemeContext';
+import { useRouter } from 'next/router';
+import { useState, useEffect } from 'react';
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const handleStart = () => setLoading(true);
+    const handleStop = () => setLoading(false);
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleStop);
+    router.events.on('routeChangeError', handleStop);
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleStop);
+      router.events.off('routeChangeError', handleStop);
+    };
+  }, [router]);
+
   return (
-    <ThemeProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <>
+      {loading && (
+        <div className="page-loader">
+          <div className="loader-dots">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+      )}
+      <ThemeProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </>
   );
 }
